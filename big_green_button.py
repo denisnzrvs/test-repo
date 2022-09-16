fdf1a74cf100544bb36e7fd69e3cf2d21c2158ce
Denis Nazarovs <denis.nazarovs@va.lv>
2022-09-16 12:56:24 +0000

    New green button added

---
+++ big_green_button.py
@@ -0,0 +1,1 @@
+A new, big, green button! Plays a different sound than the red button.
--- big_red_button.py
+++ big_red_button.py
@@ -1,2 +1,3 @@
 An exciting, now released new feature - big red button!
 No bugs or vulnerabilities here!
+Addition: now button plays a sound when pressed.
